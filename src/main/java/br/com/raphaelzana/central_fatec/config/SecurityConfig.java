597f376954b071a44b94b10a20950a3e87aef48a
Raphael Zanarelli <raphael.zanarelli@s2it.com.br>
2018-11-02 21:41:25 -0300

    backend funfando para: cadastro objeto, cadastro noticias, consulta noticia e consulta objeto (nao paginados), e tambem login autenticando tudo top

--- src/main/java/br/com/raphaelzana/central_fatec/config/SecurityConfig.java
+++ src/main/java/br/com/raphaelzana/central_fatec/config/SecurityConfig.java
@@ -32,16 +32,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
-	/*private static final String[] PUBLIC_MATCHERS_GET = {
-			"/noticias/**",
-			"/objetos/**"
-	};*/
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			
+			
+	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
 			"/usuarios/**"
 	};
 	
 	private static final String[] PUBLIC_MATCHERS = {
+			"/noticias/**",
+			"/objetos/**",
 			"/h2-console/**"
 	};
 	
@@ -51,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-			/*.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()*/
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
