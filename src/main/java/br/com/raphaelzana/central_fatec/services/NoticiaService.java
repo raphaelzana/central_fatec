8799947b033c28fcf34980ccfeb7d04167e97d1e
Raphael Zanarelli <raphael.zanarelli@s2it.com.br>
2018-11-02 22:48:30 -0300

    paginacao noticias e objetos

--- src/main/java/br/com/raphaelzana/central_fatec/services/NoticiaService.java
+++ src/main/java/br/com/raphaelzana/central_fatec/services/NoticiaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +26,11 @@ public class NoticiaService {
 	
 	public List<Noticia> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Noticia> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageResquest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageResquest);
 	}
 
 	@Transactional
